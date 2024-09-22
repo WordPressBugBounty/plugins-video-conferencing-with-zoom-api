b008ce7ce611b36ccf136da81572a331cc38db80
Sean Murphy <sean@iamseanmurphy.com>
2024-09-22 16:48:15 +0000

    4.5.3

---
+++ video-conferencing-with-zoom-api/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'techies23/video-conferencing-with-zoom-api',
+        'pretty_version' => '4.5.3',
+        'version' => '4.5.3.0',
+        'reference' => 'f7cbab819f8420771615573d753dadb73b1f7330',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.9.0',
+            'version' => '6.9.0.0',
+            'reference' => 'f03270e63eaccf3019ef0f32849c497385774e11',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'techies23/video-conferencing-with-zoom-api' => array(
+            'pretty_version' => '4.5.3',
+            'version' => '4.5.3.0',
+            'reference' => 'f7cbab819f8420771615573d753dadb73b1f7330',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
